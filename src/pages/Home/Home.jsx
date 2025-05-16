3498071dbc1081ff01f6d6b3ccaf54f8ec867351
joaogabriel <joaogabriel>
2025-05-16 11:22:59 -0300

    fim max

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
 
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Pokédex</h1>
+      <h1 className="text-3xl font-bold mb-4">Pokemons do Jaozin</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
         {pokemons.map((pokemon, index) => (
           <Link
